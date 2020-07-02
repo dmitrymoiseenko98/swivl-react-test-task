24d82f97131dca1b0e9e871927754af0c12d2548
Dmitry Moiseenko <dmitrymoiseenko@outlook.com>
2020-07-02 18:13:36 +0300

    Add info on user page

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,32 +1,104 @@
 
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUser, clearUser } from '../../store/actions';
 import Loader from '../Loader';
 import './User.css';
 
-function User(props) {
-  useEffect(() => {
-    props.fetchUser(props.match.params.login);
+class User extends Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.match.params.login);
+  }
 
-    return props.clearUser;
-  }, []);
+  componentWillUnmount() {
+    this.props.clearUser();
+  }
 
-  return (
-    <div className="User">
-      {props.loading ? (
-        <div className="User-loading">
-          <Loader />
-        </div>
-      ) : props.error ? (
-        <div className="User-error">Loading error</div>
-      ) : (
-        <div className="User-content">
-          <img className="User-avatar" src={props.user.avatar_url} alt="Avatar"/>
-        </div>
-      )}
-    </div>
-  );
+  renderUserInfo() {
+    const info = [
+      {
+        id: 0,
+        label: 'Name',
+        value: this.props.user.name
+      },
+      {
+        id: 1,
+        label: 'Followers',
+        value: this.props.user.followers
+      },
+      {
+        id: 2,
+        label: 'Following',
+        value: this.props.user.following
+      },
+      {
+        id: 3,
+        label: 'Created at',
+        value: this.props.user.created_at
+      },
+      {
+        id: 4,
+        label: 'Company',
+        value: this.props.user.company
+      },
+      {
+        id: 5,
+        label: 'Email',
+        value: this.props.user.email
+      },
+      {
+        id: 6,
+        label: 'Location',
+        value: this.props.user.location
+      },
+      {
+        id: 7,
+        label: 'Blog',
+        value: this.props.user.blog
+      },
+      {
+        id: 8,
+        label: 'Bio',
+        value: this.props.user.bio
+      }
+    ];
+
+    return (
+      <ul className="User-info">
+        {info.map(item => (
+          item.value !== null ? (
+            <li
+              className="User-info-item"
+              key={item.id}
+            >
+              {item.label}: {item.value}
+            </li>
+          ) : (
+            null
+          )
+        ))}
+      </ul>
+    );
+  }  
+
+  render() {
+    return (
+      <div className="User">
+        {this.props.loading ? (
+          <div className="User-loading">
+            <Loader />
+          </div>
+        ) : this.props.error ? (
+          <div className="User-error">Loading error</div>
+        ) : (
+          <div className="User-content">
+            <img className="User-avatar" src={this.props.user.avatar_url} alt="Avatar"/>
+            {this.renderUserInfo()}
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
